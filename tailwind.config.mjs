3fd351eac4d0e830a11c936aab813cdb332ad890
Lucas Gonzalez <luc4s92.g@gmail.com>
2026-02-20 18:14:57 -0300

    Replace theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'primary-black': '#252e46',
-				'secondary-white': '#c7c7c7',
-				'primary-blue': '#3a4e7a',
-				'secondary-blue': '#3e558f',
-				'tertiary-blue': '#4767ae',
-				'light-blue': '#82aed8',
-			  },
-			backgroundImage: {
-				"hero-pattern": "url('/mosaic.png')",
-			  },
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-black": "#090005", // fondo principal casi negro violeta
+        "secondary-white": "#d1d1db", // texto suave, no blanco puro
+
+        "primary-blue": "#5a3ea6", // violeta oscuro principal
+        "secondary-blue": "#4b2f91", // violeta más profundo (hover)
+        "tertiary-blue": "#372166", // violeta muy oscuro (detalles)
+        "light-blue": "#7b5fc9",
+        "purple-primary": "#9D00FF", // violeta apagado (highlights)
+        "purple-secundary": "#8A00FF", // violeta apagado (highlights)
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/mosaic.png')",
+      },
+    },
+  },
+  plugins: [],
+};
